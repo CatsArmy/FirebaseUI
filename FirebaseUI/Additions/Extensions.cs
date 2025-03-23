edc9b4c26154aa79f764e3555d44bbca24f1ee41
Itai Bergman <bergman.itai@gmail.com>
2025-03-24 00:02:54 +0200

    small refactor

--- FirebaseUI/Additions/Extensions.cs
+++ FirebaseUI/Additions/Extensions.cs
@@ -1,6 +1,6 @@
 ﻿namespace FirebaseUI;
 
-public static class Extensions
+internal static class Extensions
 {
     public static Java.Lang.Class Class(this Type type) => Java.Lang.Class.FromType(type);
 }
